4c2b7140af04e1992e5bc99c1c35a1719795cd3b
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2015-04-16 22:36:25 +1000

    add team page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'team/index'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -13,6 +11,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :attendees, except: [:index, :show]
+  resources :team, only: [:index]
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
