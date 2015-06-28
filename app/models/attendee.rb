8e0924d0cede68ff18c2a989b370eb7c2fbedba4
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2015-06-28 11:02:25 +1000

    fixed form styles for attendee

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,3 +1,6 @@
 class Attendee < ActiveRecord::Base
 	belongs_to :class_schedule
+  validates :age, numericality: true
+  validates :email, presence: true, email: true
+  validates :name, presence: true
 end
