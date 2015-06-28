f40505e554ccb65e4c355017183ac29873175acb
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2015-06-28 12:34:29 +1000

    removed details page and merged with book

--- app/admin/class_schedule.rb
+++ app/admin/class_schedule.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register ClassSchedule do
       f.input :title, label: "title"
       f.input :date, as: :datepicker, label: "Date"
       f.input :spots, as: :number, label: "Spots"
-      f.input :detail, as: :ckeditor, label: "Details"
+      f.input :detail, as: :ckeditor, label: "Details", toolbar: 'mini'
       f.input :start_time, as: :datetime_select, label: "Start Time", start_year: Date.today.year
       f.input :end_time, as: :datetime_select, label: "End Time", start_year: Date.today.year
       f.input :instructor_id, as: :select, collection: Instructor.pluck(:name, :id), label: "Instructor"
