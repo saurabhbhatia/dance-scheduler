b791c868bfb4e861179d2627903b21425bd64634
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2015-08-02 00:41:15 +1000

    add app level redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :set_api_keys
+  before_action :redirect_subdomain
 
   def set_api_keys
     @api_key = ENV['STRIPE_TEST_KEY']
     @recaptcha_key = ENV['CAPTCHA_KEY']
+  end
+
+  def redirect_subdomain
+    if request.host == 'www.marigolddancestudio.com'
+      redirect_to 'http://marigolddancestudio.com' + request.fullpath
+    end
   end
 end
